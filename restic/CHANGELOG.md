e0bebe838233f223e3c95128f4ff1e4401a4e2f7
brmo <brian.morris.personal@gmail.com>
2024-10-22 14:38:52 -0700

    Add restic

---
+++ restic/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [0.6] - 2024-02-04
+### Updated
+- Updated config.json to fix s6
+
---
+++ restic/run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/with-contenv bashio
+echo "HassOS restic add-on starting"
+date
+set -e
+
+mkdir -p /data/restic-cache
+
+# find and mount the data partition
+#udevd --daemon && sleep 0.5
+#udevadm trigger && sleep 0.5
+#mkdir /hassos-data
+#mount /dev/disk/by-label/hassos-data /hassos-data/
+
+# load configuration
+CONFIG_PATH=/data/options.json
+$(jq -r \
+  '.env_vars|to_entries[]|"export " + .key + "=" + (.value|tostring) + ""' \
+  "$CONFIG_PATH")
+jq -r \
+  '.exclude_patterns[]|.' \
+  "$CONFIG_PATH" > /exclude_file
+# run the backup
+(
+  #cd /hassos-data/supervisor
+  restic \
+    --cache-dir=/data/restic-cache \
+    --verbose \
+    --exclude-file=/exclude_file \
+    backup /config /ssl /backup
+)
+
+echo "Done"
