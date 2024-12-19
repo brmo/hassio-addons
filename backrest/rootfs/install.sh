9f0c7c8b6c6b64b0582e03b79ac5a5f80364604a
brmo <brian.morris.personal@gmail.com>
2024-12-19 16:33:53 +0000

    Trying again to build this!

--- backrest/rootfs/install.sh
+++ backrest/rootfs/install.sh
@@ -2,12 +2,11 @@
 
 if [ $TARGETARCH = "arm64" ]; then \
         mkdir /tmp/backrest/ && cd /tmp/ && wget https://github.com/garethgeorge/backrest/releases/download/v1.6.2/backrest_Linux_arm64.tar.gz && \
-        cd /tmp/backrest/ && tar -zxvf /tmp/backrest_Linux_arm64.tar.gz && cp /tmp/backrest/backrest /usr/bin/backrest \
-    ; fi
+        cd /tmp/backrest/ && tar -zxvf /tmp/backrest_Linux_arm64.tar.gz && cp /tmp/backrest/backrest /usr/bin/backrest
     
-if [ $TARGETARCH = "amd64" ]; then \
+elif [ $TARGETARCH = "amd64" ]; then \
         mkdir /tmp/backrest/ && cd /tmp/ && wget https://github.com/garethgeorge/backrest/releases/download/v1.6.2/backrest_Linux_x86_64.tar.gz && \
-        cd /tmp/backrest/ && tar -zxvf /tmp/backrest_Linux_x86_64.tar.gz && cp /tmp/backrest/backrest /usr/bin/backrest \
+        cd /tmp/backrest/ && tar -zxvf /tmp/backrest_Linux_x86_64.tar.gz && cp /tmp/backrest/backrest /usr/bin/backrest
     ; fi    
 
 /usr/bin/backrest --install-deps-only
