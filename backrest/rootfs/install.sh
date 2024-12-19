f6853ddc8b7087c8a1a25fda77776bd52976516e
brmo <brian.morris.personal@gmail.com>
2024-12-19 16:36:22 +0000

    Adding echo var.

--- backrest/rootfs/install.sh
+++ backrest/rootfs/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo $TARGETARCH
+
 if [ $TARGETARCH = "arm64" ]; then \
         mkdir /tmp/backrest/ && cd /tmp/ && wget https://github.com/garethgeorge/backrest/releases/download/v1.6.2/backrest_Linux_arm64.tar.gz && \
         cd /tmp/backrest/ && tar -zxvf /tmp/backrest_Linux_arm64.tar.gz && cp /tmp/backrest/backrest /usr/bin/backrest
