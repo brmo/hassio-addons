3ae5a5ef30949d57abcbbb70013d06ec0a86620d
brmo <brian.morris.personal@gmail.com>
2024-12-19 16:17:42 +0000

    Missed double ampersands.

--- backrest/rootfs/install.sh
+++ backrest/rootfs/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ $BUILDARCH = "arm64" ]; then \
-        mkdir /tmp/backrest/ && cd /tmp/ && wget https://github.com/garethgeorge/backrest/releases/download/v1.6.2/backrest_Linux_arm64.tar.gz \
+        mkdir /tmp/backrest/ && cd /tmp/ && wget https://github.com/garethgeorge/backrest/releases/download/v1.6.2/backrest_Linux_arm64.tar.gz && \
         cd /tmp/backrest/ && tar -zxvf /tmp/backrest_Linux_arm64.tar.gz && cp /tmp/backrest/backrest /usr/bin/backrest \
     ; fi
     
 if [ $BUILDARCH = "amd64" ]; then \
-        mkdir /tmp/backrest/ && cd /tmp/ && wget https://github.com/garethgeorge/backrest/releases/download/v1.6.2/backrest_Linux_x86_64.tar.gz \
+        mkdir /tmp/backrest/ && cd /tmp/ && wget https://github.com/garethgeorge/backrest/releases/download/v1.6.2/backrest_Linux_x86_64.tar.gz && \
         cd /tmp/backrest/ && tar -zxvf /tmp/backrest_Linux_x86_64.tar.gz && cp /tmp/backrest/backrest /usr/bin/backrest \
     ; fi    
 
