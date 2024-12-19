aba4c2dc30c6fad844e32c5be3b7e0cef802e061
brmo <brian.morris.personal@gmail.com>
2024-12-19 16:13:50 +0000

    Moving installation of backrest to script.

---
+++ backrest/rootfs/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $BUILDARCH = "arm64" ]; then \
+        mkdir /tmp/backrest/ && cd /tmp/ && wget https://github.com/garethgeorge/backrest/releases/download/v1.6.2/backrest_Linux_arm64.tar.gz \
+        cd /tmp/backrest/ && tar -zxvf /tmp/backrest_Linux_arm64.tar.gz && cp /tmp/backrest/backrest /usr/bin/backrest \
+    ; fi
+    
+if [ $BUILDARCH = "amd64" ]; then \
+        mkdir /tmp/backrest/ && cd /tmp/ && wget https://github.com/garethgeorge/backrest/releases/download/v1.6.2/backrest_Linux_x86_64.tar.gz \
+        cd /tmp/backrest/ && tar -zxvf /tmp/backrest_Linux_x86_64.tar.gz && cp /tmp/backrest/backrest /usr/bin/backrest \
+    ; fi    
+
+/usr/bin/backrest --install-deps-only
